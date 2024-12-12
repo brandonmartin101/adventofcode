1c1e1efd5da6804f8b0077e671fd090d9e082e47
bmartin-dealersocket <bmartin@dealersocket.com>
2024-12-12 09:10:31 -0600

    Solved 2023 3a

--- 2023/3a.ts
+++ 2023/3a.ts
@@ -1,69 +1,50 @@
 import { puzzleInput, puzzleDemo } from './inputs/3.ts';
+// const data = puzzleDemo.split('\n');
 const data = puzzleInput.split('\n');
 
-//* find position of all non-period symbols
-type Symbol = { row: number; col: number; symbol: string };
-let symbols: Symbol[] = [];
-
-for (let row = 0; row < data.length; row++) {
-	for (let col = 0; col < data[row].length; col++) {
-		if (isNaN(parseInt(data[row][col])) && data[row][col] !== '.') {
-			// console.log('symbol found', data[row][col], col);
-			symbols.push({
-				row: row,
-				col: col,
-				symbol: data[row][col],
-			});
+const getValidNumbers = () => {
+	let numbersList: number[] = [];
+	let numbersSum = 0;
+	let partNumberVerified = false;
+	let numberTemp = '';
+	for (let row = 0; row < data.length; row++) {
+		for (let col = 0; col < data[row].length; col++) {
+			if (!isNaN(parseInt(data[row][col]))) {
+				numberTemp += data[row][col];
+				if (!partNumberVerified) {
+					if (
+						isSymbol(data[row - 1][col]) ||
+						isSymbol(data[row + 1][col]) ||
+						isSymbol(data[row][col - 1]) ||
+						isSymbol(data[row][col + 1]) ||
+						isSymbol(data[row - 1][col - 1]) ||
+						isSymbol(data[row - 1][col + 1]) ||
+						isSymbol(data[row + 1][col - 1]) ||
+						isSymbol(data[row + 1][col + 1])
+					) {
+						partNumberVerified = true;
+					}
+				}
+			}
+			if (isNaN(parseInt(data[row][col])) && numberTemp !== '') {
+				if (partNumberVerified) {
+					console.log(numberTemp, 'valid');
+					numbersList.push(parseInt(numberTemp));
+					numbersSum += parseInt(numberTemp);
+				} else {
+					console.log(numberTemp, 'not valid');
+				}
+				numberTemp = '';
+				partNumberVerified = false;
+			}
 		}
 	}
-}
-
-//* find numbers and their positions next to symbols
-type PartNumbers = [row: number, colStart: number, colEnd: number, number: number];
-let partNumbers: PartNumbers[] = [];
-const addValidNumber = (row: number, col: number) => {
-	// console.log(row, col, data[row][col]);
-	let colStart = col;
-	while (true) {
-		if (data[row][colStart - 1] !== '.' && !isNaN(parseInt(data[row][colStart]))) {
-			colStart--;
-		} else {
-			break;
-		}
-		if (colStart < 1) break;
-	}
-	let colEnd = col;
-	while (true) {
-		if (data[row][colEnd] !== '.' && !isNaN(parseInt(data[row][colEnd]))) {
-			colEnd++;
-		} else {
-			break;
-		}
-		if (colEnd > data[row].length) break;
-	}
-	partNumbers.push([row, colStart, colEnd, parseInt(data[row].substring(colStart, colEnd))]);
+	return [numbersList, numbersSum];
 };
 
-for (let symbol of symbols) {
-	// console.log(symbol.row, symbol.col, symbol.symbol);
-	if (data[symbol.row - 1][symbol.col - 1] !== '.') addValidNumber(symbol.row - 1, symbol.col - 1);
-	if (data[symbol.row - 1][symbol.col] !== '.') addValidNumber(symbol.row - 1, symbol.col);
-	if (data[symbol.row - 1][symbol.col + 1] !== '.') addValidNumber(symbol.row - 1, symbol.col + 1);
-	if (data[symbol.row][symbol.col - 1] !== '.') addValidNumber(symbol.row, symbol.col - 1);
-	if (data[symbol.row][symbol.col + 1] !== '.') addValidNumber(symbol.row, symbol.col + 1);
-	if (data[symbol.row + 1][symbol.col - 1] !== '.') addValidNumber(symbol.row + 1, symbol.col - 1);
-	if (data[symbol.row + 1][symbol.col] !== '.') addValidNumber(symbol.row + 1, symbol.col);
-	if (data[symbol.row + 1][symbol.col + 1] !== '.') addValidNumber(symbol.row + 1, symbol.col + 1);
-}
-// console.log(partNumbers);
+const isSymbol = (char: string) => {
+	if (char === undefined) return false;
+	return !char.match(/[0-9.]/);
+};
 
-// remove duplicates
-partNumbers = partNumbers.filter((partNumber, index, self) => self.findIndex((t) => t[3] === partNumber[3]) === index);
-console.log(partNumbers);
-
-// sum the numbers (check for duplicates)
-let sum = 0;
-for (let partNumber of partNumbers) {
-	sum += partNumber[3];
-}
-console.log(sum);
+console.log(getValidNumbers());
